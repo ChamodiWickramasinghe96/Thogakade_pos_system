fea634b72de11f17e3a898527e884f5156680dc8
ChamodiWickramasinghe96 <chamodi1002@gmail.com>
2025-10-24 19:36:02 +0530

    Initial Commit

---
+++ src/main/java/Start.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Start extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/Customer.fxml"))));
+        stage.show();
+
+
+    }
+}
---
+++ src/main/java/controller/CustomerController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerController {
+    public TextField txtId;
+    public TextField txtAddress;
+    public TextField txtName;
+    public TextField txtSalary;
+    public TableView tblCustomer;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+
+    public void btnAddOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRealoadOnAction(ActionEvent actionEvent) {
+    }
+}
