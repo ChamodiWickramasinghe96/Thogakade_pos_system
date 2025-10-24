f304edde33567382e53ea37422f650c2bbbab114
ChamodiWickramasinghe96 <chamodi1002@gmail.com>
2025-10-24 22:02:11 +0530

    Create Dashboard

---
+++ src/main/java/controller/DashRootController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class DashRootController {
+    public void btnDashboardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+    }
+}
