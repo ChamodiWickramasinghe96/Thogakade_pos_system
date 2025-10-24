8157dd63d27d5ff87a2c63cf3e8e6cc11f04fc70
ChamodiWickramasinghe96 <chamodi1002@gmail.com>
2025-10-24 23:28:49 +0530

    Create Order Form

---
+++ src/main/java/controller/OrderController.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class OrderController {
+    public TextField txtOrderId;
+    public TextField txtPrice;
+    public TextField txtItemCode;
+    public TextField txtCustomerId;
+    public TableView tblOrder;
+    public TableColumn colOrerId;
+    public TableColumn colCustomerId;
+    public TableColumn colItemCode;
+    public TableColumn colPrice;
+
+    public void btnAddOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnReloadOrderOnAction(ActionEvent actionEvent) {
+    }
+}
